8cd20498595cf3338fe01eba85b8282853558723
thchara <thcharara@gmail.com>
2025-04-28 16:17:16 +0100

    Update AudioTest.tsx

--- src/public/viz-guardrails/AudioTest.tsx
+++ src/public/viz-guardrails/AudioTest.tsx
@@ -2,63 +2,93 @@
 /* eslint-disable no-nested-ternary */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import {
-  Center, Stack, Text,
-} from '@mantine/core';
-import {
-  useEffect,
-} from 'react';
-import { StimulusParams } from '../../store/types';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-nested-ternary */
+
+import React, { useEffect } from 'react';
+import { Center, Stack, Text } from '@mantine/core';
 import RecordingAudioWaveform from '../../components/interface/RecordingAudioWaveform';
+import { StimulusParams } from '../../store/types';
+
+// NEW imports for manual validation dispatch
+import { useStoreDispatch, useStoreActions } from '../../store/store';
+import { useCurrentComponent, useCurrentStep } from '../../routes/utils';
 
 export function AudioTest({ parameters, setAnswer }: StimulusParams<any>) {
+  const dispatch = useStoreDispatch();
+  const { updateResponseBlockValidation } = useStoreActions();
+  const currentComponent = useCurrentComponent();
+  const currentStep = useCurrentStep();
+  const identifier = `${currentComponent}_${currentStep}`;
+
   useEffect(() => {
-    const _stream = navigator.mediaDevices.getUserMedia({
-      audio: true,
-    });
+    let animationId: number;
+    let soundDetected = false;
 
-    _stream.then((stream) => {
-      const mediaRecorder = new MediaRecorder(stream);
-      mediaRecorder.start();
+    async function init() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const audioContext = new AudioContext();
+        await audioContext.resume();
 
-      const audioContext = new AudioContext();
-      const audioStreamSource = audioContext.createMediaStreamSource(stream);
-      const analyser = audioContext.createAnalyser();
+        // (Optional) amplify mic input a bit
+        const source = audioContext.createMediaStreamSource(stream);
+        const gainNode = audioContext.createGain();
+        gainNode.gain.value = 3;
+        source.connect(gainNode);
 
-      analyser.minDecibels = -45;
-      audioStreamSource.connect(analyser);
+        const analyser = audioContext.createAnalyser();
+        analyser.fftSize = 256;
+        gainNode.connect(analyser);
 
-      const bufferLength = analyser.frequencyBinCount;
-      const domainData = new Uint8Array(bufferLength);
+        const buffer = new Uint8Array(analyser.fftSize);
 
-      let soundDetected = false;
+        const detect = () => {
+          analyser.getByteTimeDomainData(buffer);
 
-      const detectSound = () => {
-        if (soundDetected) {
-          return;
-        }
+          // compute max deviation from silence (128)
+          let maxDelta = 0;
+          for (let i = 0; i < buffer.length; i += 1) {
+            const delta = Math.abs(buffer[i] - 128);
+            if (delta > maxDelta) maxDelta = delta;
+          }
 
-        analyser.getByteFrequencyData(domainData);
-
-        for (let i = 0; i < bufferLength; i += 1) {
-          if (domainData[i] > 0) {
+          // tuning: fire on any non-zero deviation
+          if (!soundDetected && maxDelta > 15) {
             soundDetected = true;
+
+            // 1) update your hidden-radio (sidebar)
             setAnswer({
               status: true,
               provenanceGraph: undefined,
-              answers: {
-                audioTest: true,
-              },
+              answers: { audioTest: 'yes' },
             });
+
+            // 2) also validate the belowStimulus block for NextButton
+            dispatch(updateResponseBlockValidation({
+              location: 'belowStimulus',
+              identifier,
+              status: true,
+              provenanceGraph: undefined,
+              values: { audioTest: 'yes' },
+            }));
+
+            cancelAnimationFrame(animationId);
+            return;
           }
-        }
 
-        window.requestAnimationFrame(detectSound);
-      };
+          animationId = requestAnimationFrame(detect);
+        };
 
-      window.requestAnimationFrame(detectSound);
-    });
-  }, [setAnswer]);
+        detect();
+      } catch (err) {
+        console.error('AudioTest init error', err);
+      }
+    }
+
+    init();
+    return () => cancelAnimationFrame(animationId);
+  }, [dispatch, identifier, setAnswer, updateResponseBlockValidation]);
 
   return (
     <Center style={{ height: '70%', width: '100%' }}>
@@ -72,7 +102,9 @@ export function AudioTest({ parameters, setAnswer }: StimulusParams<any>) {
         <Text ta="center" fw={700}>
           If you are not comfortable or able to speak English during this study, please return the study.
         </Text>
-        <Center><RecordingAudioWaveform height={200} width={400} /></Center>
+        <Center>
+          <RecordingAudioWaveform height={200} width={400} />
+        </Center>
       </Stack>
     </Center>
   );
